ce730ad355d04289692f5e6d8e5d2bca214672ed
Jose Luis Castello <wbo_jlcastello@ext.laliga.com>
2024-09-17 12:26:22 +0200

    First commit

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Pricing Page Title',
+    description: 'Pricing Page Description',
+};
+
+export default function PricingPage() {
+
+    return (
+
+        <>
+            <span className="text-7xl">Pricing Page</span>
+        </>
+
+    )
+
+}
